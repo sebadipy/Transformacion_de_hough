c0fc1a341ec8aabf05239cc82c6636ee5d006209
Sebastián DI Pierro <sebadipy@hotmail.com>
2022-06-11 18:08:09 -0300

    Tercer Commit

--- Transformación de  Hough Circulos.py
+++ Transformación de  Hough Circulos.py
@@ -8,7 +8,7 @@ ALUMNO Di Pierro, Sebastián Miguel. (VINF07402).
 import cv2
 import numpy as np
  
-img = cv2.imread('opencv_logo.png',0)
+img = cv2.imread('motor circunsferencia.png',0)
 img = cv2.medianBlur(img,5)
 cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
  
--- Transformación de  Hough Rectas hough.py
+++ Transformación de  Hough Rectas hough.py
@@ -10,7 +10,7 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread('SUDOKU.png')
+img = cv2.imread('motor recta.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 cv2.imshow('edges', edges)
