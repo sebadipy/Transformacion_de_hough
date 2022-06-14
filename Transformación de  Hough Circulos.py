ff0fb06a6632e72effaf97ebc6378b628fde6667
Sebastián DI Pierro <sebadipy@hotmail.com>
2022-06-13 22:25:45 -0300

    quinto commit

--- Transformación de  Hough Circulos.py
+++ Transformación de  Hough Circulos.py
@@ -16,9 +16,9 @@ circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRa
  
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
-    # draw the outer circle
+
     cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
+
     cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
 cv2.imshow('Resultado de la imagen',cimg)
--- Transformación de  Hough Rectas hough.py
+++ Transformación de  Hough Rectas hough.py
@@ -22,13 +22,13 @@ for line in lines:
     b = np.sin(theta)
     x0 = a * rho
     y0 = b * rho
-    # x1 stores the rounded off value of (r * cos(theta) - 1000 * sin(theta))
+
     x1 = int(x0 + 1000 * (-b))
-    # y1 stores the rounded off value of (r * sin(theta)+ 1000 * cos(theta))
+
     y1 = int(y0 + 1000 * (a))
-    # x2 stores the rounded off value of (r * cos(theta)+ 1000 * sin(theta))
+
     x2 = int(x0 - 1000 * (-b))
-    # y2 stores the rounded off value of (r * sin(theta)- 1000 * cos(theta))
+
     y2 = int(y0 - 1000 * (a))
     cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
